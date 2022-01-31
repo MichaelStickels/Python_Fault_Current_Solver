075a29c8ad9f8755d3d7b4245387509de3681088
Michael Stickels <stickelsmt@gmail.com>
2022-01-31 08:52:50 -0800

    Read in xlsx data file

--- solver.py
+++ solver.py
@@ -12,4 +12,24 @@
 # ---------------------------------------------------------------------------
 
 
+
+# Parameters and Constants
+data_path = "data/CHW1Data.xlsx"
+
+
+
 # Imports
+import numpy as np
+import pandas as pd
+
+
+
+# Read in excel input file
+input = pd.read_excel(data_path, sheet_name=None)
+busData = input['BusData']
+lineData = input['LineData']
+faultData = input['FaultData']
+
+print(busData)
+print(lineData)
+print(faultData)
